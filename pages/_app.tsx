b6f8e481e6c331f72261326ab3d63c08eebc2eff
our408 <our@408.co.kr>
2022-05-08 00:08:03 +0900

    moving

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,38 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
 
 import type { AppProps } from 'next/app'
+import { isMobile as isMobileDetect } from 'react-device-detect'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  function setScreenSize() {
-    let vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
-  }
-  useEffect(() => {
-    setScreenSize()
-  })
+  const [vh, setVh] = useState<number>(0)
+  const [vw, setVw] = useState<number>(0)
+  const [isMobile, setIsMobile] = useState<boolean>(true)
 
-  return <Component {...pageProps} />
+  useEffect(() => {
+    setIsMobile(isMobileDetect)
+  }, [])
+
+  useEffect(() => {
+    setVh(window.innerHeight)
+    setVw(window.innerWidth)
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>CHEESYLAZY</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
+
+      <Component {...pageProps} vh={vh} vw={vw} isMobile={isMobile} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,32 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
-import eating from '../resources/img/eating_v2.gif'
+//import eating from '../resources/img/eating_v2.gif'
+import eating from '../resources/img/eating_v3.gif'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-const Home: NextPage = () => {
+interface HomeProps {
+  vh: number
+  isMobile: boolean
+}
+
+const Home: NextPage<HomeProps> = ({ vh, isMobile }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/menu')
+    }, 2000)
+  }, [])
+
   return (
     <div
-      className="w-full flex justify-center items-center"
+      className={`w-full flex justify-center items-center `}
       style={{
-        height: `calc(var(--vh, 1vh) * 100)`,
+        height: vh + 'px',
       }}
     >
-      <Image src={eating} width={100} height={150} />
+      <Image src={'/eating_v3.gif'} width={100} height={180} />
     </div>
   )
 }
