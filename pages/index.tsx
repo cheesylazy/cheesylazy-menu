665ef1520e59cdf9495594132bb43d3673fc7898
our408 <our@408.co.kr>
2022-05-18 17:23:34 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,20 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
-//import eating from '../resources/img/eating_v2.gif'
-import eating from '../resources/img/eating_v3.gif'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 interface HomeProps {
   vh: number
   isMobile: boolean
+  data: object
 }
 
-const Home: NextPage<HomeProps> = ({ vh, isMobile }) => {
-  const router = useRouter()
+const Home: NextPage<HomeProps> = ({ vh, isMobile, data }) => {
+  const [display, setDisplay] = useState<String>('block')
 
   useEffect(() => {
     setTimeout(() => {
-      router.push('/menu')
+      setDisplay('none')
     }, 2000)
   }, [])
 
@@ -26,9 +25,112 @@ const Home: NextPage<HomeProps> = ({ vh, isMobile }) => {
         height: vh + 'px',
       }}
     >
-      <Image src={'/eating_v3.gif'} width={100} height={180} />
+      <div style={{ display: display }}>
+        <Image src={'/eating_v3.gif'} width={100} height={180} />
+      </div>
+      <div
+        className={`w-full h-full ${isMobile ? 'p-3' : 'p-10'} pt-2`}
+        style={{ display: display === 'none' ? 'block' : 'none' }}
+      >
+        <header
+          className={`relative max-w-[640px] w-[70vw] max-h-[154px] h-[18vw] m-auto ${
+            isMobile ? 'mb-6' : ''
+          }`}
+        >
+          <Image src={'/logo.png'} layout="fill" objectFit="fill" />
+        </header>
+
+        <main className={`flex flex-wrap w-full`}>
+          {/* SADNWICH */}
+          <div className={`${isMobile ? 'w-[100%]' : 'w-[50%] p-10'}`}>
+            <div
+              className={`${
+                isMobile ? 'text-3xl' : 'text-7xl'
+              } mb-7 border-b border-solid border-black`}
+            >
+              SANDWICH
+            </div>
+            {data?.sandwich.map((obj) => {
+              return (
+                <div className="menu mb-12">
+                  <div className="flex flex-wrap justify-between items-center mb-3">
+                    <div className="text-3xl">{obj.title}</div>
+                    <div className="text-xl">{obj.price}</div>
+                  </div>
+
+                  <li>{obj.bread}</li>
+                  <li>{obj.sauce}</li>
+                  <li>{obj.ingredients}</li>
+                  {obj.etc && <li>{obj.etc}</li>}
+                </div>
+              )
+            })}
+          </div>
+
+          {/* NON - SADNWICH */}
+          <div className={`${isMobile ? 'w-[100%] mb-10' : 'w-[50%] p-10'}`}>
+            <div
+              className={`${
+                isMobile ? 'text-3xl mt-5' : 'text-7xl'
+              } mb-7 border-b border-solid border-black`}
+            >
+              NON-SANDWICH
+            </div>
+
+            {data?.['non-sandwich'].map((obj) => {
+              return (
+                <div className="menu mb-6">
+                  <div className="flex flex-wrap justify-between items-center mb-1">
+                    <div className="text-3xl">{obj.title}</div>
+                    <div className="text-xl">{obj.price}</div>
+                  </div>
+
+                  {obj.bread && <li>{obj.bread}</li>}
+                  {obj.sauce && <li>{obj.sauce}</li>}
+                  {obj.ingredients && <li>{obj.ingredients}</li>}
+                  {obj.etc && <li>{obj.etc}</li>}
+                </div>
+              )
+            })}
+
+            {/* DRINK */}
+            <div
+              className={`${
+                isMobile ? 'text-3xl mt-16' : 'text-7xl mt-8'
+              } mb-7 border-b border-solid border-black`}
+            >
+              DRINK
+            </div>
+            {data?.['drink'].map((obj) => {
+              return (
+                <div className="menu mb-6">
+                  <div className="flex flex-wrap justify-between items-center mb-1">
+                    <div className="text-3xl">{obj.title}</div>
+                    <div className="text-xl">{obj.price}</div>
+                  </div>
+
+                  {obj.bread && <li>{obj.bread}</li>}
+                  {obj.sauce && <li>{obj.sauce}</li>}
+                  {obj.ingredients && <li>{obj.ingredients}</li>}
+                  {obj.etc && <li>{obj.etc}</li>}
+                </div>
+              )
+            })}
+          </div>
+        </main>
+      </div>
     </div>
   )
 }
 
 export default Home
+
+export async function getServerSideProps(context) {
+  const res = await (
+    await fetch(`https://api.408.co.kr/cheesylazy/menu`)
+  ).json()
+
+  return {
+    props: { data: res },
+  }
+}
