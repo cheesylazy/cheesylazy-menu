1716f0b8535f3e428dd88476ac42b96421fe961a
our408 <our@408.co.kr>
2022-05-19 18:17:17 +0900

    maybe final

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ interface HomeProps {
 
 const Home: NextPage<HomeProps> = ({ vh, isMobile, data }) => {
   const [display, setDisplay] = useState<string>('none')
+  const [language, setLanguage] = useState<string>('KO')
+  const [title, setTitle] = useState<string>(data.title)
 
   useEffect(() => {
     // setTimeout(() => {
@@ -30,6 +32,30 @@ const Home: NextPage<HomeProps> = ({ vh, isMobile, data }) => {
       </div>
 
       <div
+        className={`absolute ${
+          isMobile ? 'top-[40px] right-2' : 'top-[60px] right-12'
+        } `}
+      >
+        <div className="switch">
+          <input
+            id="language-toggle"
+            className="check-toggle check-toggle-round-flat"
+            type="checkbox"
+            onClick={() => {
+              if (language === 'KO') {
+                setLanguage('EN')
+              } else {
+                setLanguage('KO')
+              }
+            }}
+          />
+          <label htmlFor="language-toggle" />
+          <span className="on leading-[9px]">KO</span>
+          <span className="off leading-[9px]">EN</span>
+        </div>
+      </div>
+
+      <div
         className={`w-full h-full ${isMobile ? 'p-4 pt-10' : 'p-10 pt-14'}`}
         style={{ display: display === 'none' ? 'block' : 'none' }}
       >
@@ -37,34 +63,54 @@ const Home: NextPage<HomeProps> = ({ vh, isMobile, data }) => {
           {Object.keys(data).map((type, index) => {
             return (
               <div
-                className={`${isMobile ? 'w-[100%]' : 'w-[50%]'}`}
+                className={`${isMobile ? 'w-[100%] mb-10' : 'w-[50%]'}`}
                 key={index}
               >
                 <div
                   className={`${
-                    isMobile ? 'text-3xl' : 'text-6xl'
-                  } mb-7 font-[Montserrat] font-bold`}
+                    isMobile ? 'text-4xl mb-4' : 'text-5xl mb-7'
+                  } font-[Montserrat]`}
                 >
                   {type.toUpperCase()}
                 </div>
+
                 {data?.[type].map((obj, index) => {
+                  let title = language === 'KO' ? obj.title : obj.title_en
                   return (
                     <div
-                      className="pl-8 menu mb-12 font-[NanumSquareB]"
+                      className={`${
+                        isMobile ? 'pl-5 mb-8' : 'pl-8 mb-10'
+                      } font-[NanumSquareB]`}
                       key={index}
                     >
-                      <div className="mb-3">
-                        <span className="text-4xl font-semibold">
-                          {obj.title}
+                      <div className="mb-1">
+                        <span
+                          className={`${isMobile ? 'text-2xl' : 'text-3xl'}`}
+                        >
+                          {title}
                         </span>
-                        <span className="pl-10 text-4xl font-semibold">
-                          {numberWithCommas(obj.price)}원
+                        <span
+                          className={`${
+                            isMobile ? 'text-2xl' : 'text-3xl pl-10'
+                          } `}
+                        >
+                          {isMobile && <br></br>}
+                          {title.length > 30 && !isMobile && <br></br>}
+                          {language === 'KO' ? '' : '₩'}{' '}
+                          {numberWithCommas(obj.price)}{' '}
+                          {language === 'KO' ? '원' : ''}
                         </span>
                       </div>
 
                       {obj.description && (
-                        <div className="whitespace-pre-line text-2xl font-[NanumSquareR] text-gray-600">
-                          {obj.description}
+                        <div
+                          className={` ${
+                            isMobile ? 'text-xl' : 'text-2xl'
+                          } whitespace-pre-line font-[NanumSquareR] text-gray-600`}
+                        >
+                          {language === 'KO'
+                            ? obj.description
+                            : obj.description_en}
                         </div>
                       )}
                     </div>
