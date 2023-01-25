9994e5158920661bced2db656af839313703483f
comminco <iam@commin.co>
2023-01-25 21:35:38 +0900

    feat: 링크 텍스트에 색깔 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,70 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import type { NextPage } from "next";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 interface HomeProps {
-  vh: number
-  isMobile: boolean
+  vh: number;
+  isMobile: boolean;
 }
 
 const type2Title = {
-  food: '1. FOOD',
-  side: '2. SIDE',
-  drink: '3. DRINK',
-  'combo meal': 'COMBO MEAL',
-  'ice cream': 'ICE CREAM',
-}
+  food: "1. FOOD",
+  side: "2. SIDE",
+  drink: "3. DRINK",
+  "combo meal": "COMBO MEAL",
+  "ice cream": "ICE CREAM",
+};
 
 const Home: NextPage<HomeProps> = ({ vh, isMobile }) => {
-  const [display, setDisplay] = useState<string>('block')
-  const [data, setData] = useState({})
-  const [language, setLanguage] = useState<string>('KO')
+  const [display, setDisplay] = useState<string>("block");
+  const [data, setData] = useState({});
+  const [language, setLanguage] = useState<string>("KO");
 
   useEffect(() => {
     const init = async () => {
       const res = await (
         await fetch(`https://api.408.co.kr/cheesylazy/menu/`)
-      ).json()
-      console.log(res)
-      setData(res)
-    }
-    init()
+      ).json();
+      setData(res);
+    };
+    init();
 
     setTimeout(() => {
-      setDisplay('none')
-    }, 2100)
-  }, [])
+      setDisplay("none");
+    }, 2100);
+  }, []);
 
   return (
     <div
       className={`w-full flex justify-center items-center h-10`}
       style={{
-        height: vh + 'px',
+        height: vh + "px",
 
-        backgroundColor: display === 'none' ? '' : 'rgba(246, 229, 141,0.26)',
+        backgroundColor: display === "none" ? "" : "rgba(246, 229, 141,0.26)",
       }}
     >
-      <div style={{ display: display === 'none' ? 'none' : 'block' }}>
-        <Image src={'/eating_v3.gif'} width={324} height={576} />
+      <div style={{ display: display === "none" ? "none" : "block" }}>
+        <Image src={"/eating_v3.gif"} width={324} height={576} />
       </div>
       {/* 언어 토글 */}
       <div
         className={`absolute ${
-          isMobile ? 'top-[55px] right-2' : 'top-[40px] right-12'
+          isMobile ? "top-[55px] right-2" : "top-[40px] right-12"
         } `}
       >
         <div
           className="switch"
-          style={{ display: display === 'none' ? 'block' : 'none' }}
+          style={{ display: display === "none" ? "block" : "none" }}
         >
           <input
             id="language-toggle"
             className="check-toggle check-toggle-round-flat"
             type="checkbox"
             onClick={() => {
-              if (language === 'KO') {
-                setLanguage('EN')
+              if (language === "KO") {
+                setLanguage("EN");
               } else {
-                setLanguage('KO')
+                setLanguage("KO");
               }
             }}
           />
@@ -79,67 +78,67 @@ const Home: NextPage<HomeProps> = ({ vh, isMobile }) => {
       <div
         className={`w-full h-full`}
         style={{
-          display: display === 'none' ? 'block' : 'none',
+          display: display === "none" ? "block" : "none",
         }}
       >
         <main
           className={`flex flex-wrap w-full pt-10`}
           style={{
-            backgroundColor: 'rgba(246, 229, 141,0.26)',
+            backgroundColor: "rgba(246, 229, 141,0.26)",
           }}
         >
           {Object.keys(data).map((type, index) => {
             return (
               <div
                 className={`${
-                  isMobile ? 'w-[100%]' : 'w-[50%] min-w-[550px]'
-                } ${isMobile ? 'p-4' : 'px-10 pb-2'}`}
+                  isMobile ? "w-[100%]" : "w-[50%] min-w-[550px]"
+                } ${isMobile ? "p-4" : "px-10 pb-2"}`}
                 key={index}
               >
                 <div
                   className={`${
-                    isMobile ? 'text-4xl text-[32px] mb-4' : 'text-5xl mb-7'
+                    isMobile ? "text-4xl text-[32px] mb-4" : "text-5xl mb-7"
                   } font-[Montserrat] `}
                 >
                   <span className="">{type2Title[type]}</span>
                 </div>
 
                 {data?.[type].map((obj, index) => {
-                  let title = language === 'KO' ? obj.title : obj.title_en
+                  let title = language === "KO" ? obj.title : obj.title_en;
                   return (
                     <div
                       className={`${
-                        isMobile ? 'ml-3 mb-5' : 'ml-5 mb-10'
+                        isMobile ? "ml-3 mb-5" : "ml-5 mb-10"
                       } font-[NanumSquareB]`}
                       key={index}
                     >
                       <div className="mb-1">
                         <span
-                          className={`${isMobile ? 'text-2xl' : 'text-3xl'}`}
+                          className={`${isMobile ? "text-2xl" : "text-3xl"}`}
                         >
                           {title}
                         </span>
                         <span
                           className={`${
-                            isMobile ? 'text-2xl' : 'text-3xl ml-10'
+                            isMobile ? "text-2xl" : "text-3xl ml-10"
                           } `}
                         >
                           {isMobile && <br></br>}
                           {title.length > 30 && !isMobile && <br></br>}
-                          {language === 'KO' ? '' : '₩'}{' '}
-                          {numberWithCommas(obj.price)}{' '}
-                          {language === 'KO' ? '원' : ''}
+                          {language === "KO" ? "" : "₩"}{" "}
+                          {numberWithCommas(obj.price)}{" "}
+                          {language === "KO" ? "원" : ""}
                         </span>
                       </div>
 
                       {obj.description && (
                         <div
                           className={` ${
-                            isMobile ? 'text-xl' : 'text-2xl'
+                            isMobile ? "text-xl" : "text-2xl"
                           } whitespace-pre-line font-[NanumSquareR] text-gray-600`}
-                          style={{ marginTop: '5px' }}
+                          style={{ marginTop: "5px" }}
                         >
-                          {language === 'KO' ? (
+                          {language === "KO" ? (
                             <>{convert(obj.description)}</>
                           ) : (
                             obj.description_en
@@ -147,18 +146,18 @@ const Home: NextPage<HomeProps> = ({ vh, isMobile }) => {
                         </div>
                       )}
                     </div>
-                  )
+                  );
                 })}
               </div>
-            )
+            );
           })}
         </main>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 // export async function getServerSideProps(context) {
 //   const res = await (
@@ -171,25 +170,25 @@ export default Home
 // }
 
 function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 const convert = (str: string) => {
-  const converts = str.match(/\(.*?\)/g)
+  const converts = str.match(/\(.*?\)/g);
 
-  if (!str.includes('#')) {
-    return str
+  if (!str.includes("#")) {
+    return str;
   }
 
   if (!converts) {
-    return str
+    return str;
   }
 
   for (let text of converts) {
-    let [content, link] = text.replace('(', '').replace(')', '').split('#')
-    let link_text = `<a href=${link} target='_blank' class='underline'>${content}</a>`
-    str = str.replace(text, link_text)
+    let [content, link] = text.replace("(", "").replace(")", "").split("#");
+    let link_text = `<a href=${link} id='link_text' target='_blank' class='underline'>${content}</a>`;
+    str = str.replace(text, link_text);
   }
 
-  return <div dangerouslySetInnerHTML={{ __html: str }} />
-}
+  return <div dangerouslySetInnerHTML={{ __html: str }} />;
+};
--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -1,65 +1,63 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Image from "next/image";
 //import eating from '../resources/img/eating_v2.gif'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 interface MenuProps {
-  vh: number
-  vw: number
-  isMobile: boolean
+  vh: number;
+  vw: number;
+  isMobile: boolean;
 }
 
 const Menu: NextPage<MenuProps> = ({ vh, vw, isMobile }) => {
-  const [width, setWidth] = useState(90)
-  const [height, setHeight] = useState(50)
-  const velocity = 300
+  const [width, setWidth] = useState(90);
+  const [height, setHeight] = useState(50);
+  const velocity = 300;
   const command: {
-    [index: number]: string
+    [index: number]: string;
   } = {
     0: `translate(0px, ${vh - height * 1.5}px)`,
     1: `translate(${vw - width}px , ${vh - height * 1.5}px)`,
     2: `translate(${vw - width}px , ${height / 2}px)`,
     3: `translate(0px , ${height / 2}px)`,
-  }
+  };
   const commandRotate: {
-    [index: number]: string
+    [index: number]: string;
   } = {
     0: `rotate(0deg)`,
     1: `rotate(-90deg)`,
     2: `rotate(180deg)`,
     3: `rotate(90deg)`,
-  }
+  };
   const commandSecond: {
-    [index: number]: number
+    [index: number]: number;
   } = {
     0: vh / velocity,
     1: vw / velocity,
     2: vh / velocity,
     3: vw / velocity,
-  }
-  const [index, setIndex] = useState<number>(1)
+  };
+  const [index, setIndex] = useState<number>(1);
 
   useEffect(() => {
-    let second = commandSecond[index % 4]
-
-    console.log(vh, second)
+    let second = commandSecond[index % 4];
 
     setTimeout(() => {
-      setIndex((i) => i + 1)
-    }, second * 1000)
-  }, [index])
+      setIndex((i) => i + 1);
+    }, second * 1000);
+  }, [index]);
 
   return (
     <div
       className="relative w-full bg-white"
       style={{
-        height: vh + 'px',
+        height: vh + "px",
       }}
     >
       <div
         style={{
-          position: 'absolute',
+          position: "absolute",
           top: `0`,
           transition: `all linear ${commandSecond[index % 4]}s`,
           transform: command[index % 4],
@@ -70,11 +68,11 @@ const Menu: NextPage<MenuProps> = ({ vh, vw, isMobile }) => {
             transform: commandRotate[index % 4],
           }}
         >
-          <Image src={'/eating_v2_rotate.gif'} width={width} height={height} />
+          <Image src={"/eating_v2_rotate.gif"} width={width} height={height} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
